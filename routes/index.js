18fb19a1340f57fa2719bf2799cc0a13cf5f8ea0
jeado <jeado@bitfinder.co>
2016-06-30 22:08:42 +0900

    add todo static content

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Webframeworks.kr Angular Quickstart' });
 });
 
 module.exports = router;
