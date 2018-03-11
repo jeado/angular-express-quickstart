247035bb83b178a6059a51ecbdc9393ed9c3f773
jeado <jeado@bitfinder.co>
2018-03-11 15:25:47 +0900

    angular  success가 deprecated 되어서 수정

--- public/js/todo/todo.controller.js
+++ public/js/todo/todo.controller.js
@@ -1,51 +1,51 @@
-(function(){
-    'use strict';
+(function() {
+  "use strict";
 
-    angular
-        .module('todoApp')
-        .controller('TodoCtrl', TodoCtrl)
+  angular.module("todoApp").controller("TodoCtrl", TodoCtrl);
 
-    function TodoCtrl(TodoService, initTodos, ProjectService){    
-        var vm = this;
-        console.log(initTodos);
-        if(initTodos.project) vm.project = initTodos.project;
-        vm.title = (initTodos.project) ? initTodos.project.name + " Project" : "Inbox";
-        vm.todos = initTodos.data;  
-        vm.addTodo = function (content) {    
-            TodoService.addTodo({
-                content: content,
-                done: false,
-                project_id : (initTodos.project) ? initTodos.project.id : null    
-            })
-                .success(refresh);
-        };
+  function TodoCtrl(TodoService, initTodos, ProjectService) {
+    var vm = this;
+    console.log(initTodos);
+    if (initTodos.project) vm.project = initTodos.project;
+    vm.title = initTodos.project
+      ? initTodos.project.name + " Project"
+      : "Inbox";
+    vm.todos = initTodos.data;
+    vm.addTodo = function(content) {
+      TodoService.addTodo({
+        content: content,
+        done: false,
+        project_id: initTodos.project ? initTodos.project.id : null
+      }).then(refresh);
+    };
 
-        vm.toggleDone = function (todo, message) {            
-            TodoService.updateTodo(todo.id, {
-                done : todo.done
-            }).success(refresh);
-        }
-        
-        vm.updateTodo = function (todo) {
-            console.log(todo);       
-            TodoService.updateTodo(todo.id, {                
-                content : todo.content
-            }).success(refresh);
-        }
+    vm.toggleDone = function(todo, message) {
+      TodoService.updateTodo(todo.id, {
+        done: todo.done
+      }).then(refresh);
+    };
 
-        function refresh() {
-            vm.newContent = "";
-            if(vm.project) 
-                ProjectService.getTodosByProjectId(vm.project.id)
-                    .then(function (result) {
-                        vm.todos = result.data
-                    })
-            else 
-                TodoService.getInbox()
-                    .success(function (result) {                        
-                        vm.todos = result.data;
-                    });
-        }
+    vm.updateTodo = function(todo) {
+      console.log(todo);
+      TodoService.updateTodo(todo.id, {
+        content: todo.content
+      }).then(refresh);
+    };
+
+    function refresh(response) {
+      console.log(response);
+      vm.newContent = "";
+      if (vm.project) {
+        ProjectService.getTodosByProjectId(vm.project.id).then(function(result) {
+          console.log(result.data)
+          vm.todos = result.data.data;
+        });
+      } else {
+        TodoService.getInbox().then(function(result) {
+          console.log(result.data)
+          vm.todos = result.data.data;
+        });
+      }
     }
-
-}());
+  }
+})();
